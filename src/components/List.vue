0978863941918b95778b1c236fbd5043f213be74
Qingyang <360898257@qq.com>
2017-03-17 11:35:05 +0800

    add tokenCounter

--- src/components/List.vue
+++ src/components/List.vue
@@ -9,21 +9,20 @@
       </q-toolbar-title>
     </div>
     <div class="layout-view">
-      <button class="addButton primary outline">
+      <button v-on:click="openBox" class="addButton primary outline">
         <i>add</i>
       </button>
       <div class="list">
           <div class="item two-lines">
-
             <img class="item-primary" :src="'statics/boy-avatar.png'">
             <div class="item-content has-secondary">
-              <div>读两篇英文新闻。</div>
+              <div>{{commentTodo}}</div>
               <div>每天</div>
             </div>
             <div class="item-secondary stamp">
               未完成
             </div>
-            <q-range v-model="percentage" class="slider" :min="0" :max="1" :step="1"></q-range>
+            <!--<q-range v-model="percentage" class="slider" :min="0" :max="1" :step="1"></q-range>-->
           </div>
         </div>
     </div>
@@ -39,9 +38,60 @@
     data() {
       return {
         toolbarTitle: "我的努力",
-        percentage: 0
-
+        // percentage: 0,
+        commentTodo:'',
+        optionData: {
+          times:['everyday','everyweek','onlyOnce'],
+          difficulty: 0
+        }
       }
+    },
+
+    methods: {
+      openBox: function(){
+        Dialog.create({
+        title: '编辑详情',
+        form: {
+          comments: {
+            type: 'textarea',
+            label: '我要做的努力是：',
+            model: 'commentTodo'
+          },
+          //message:'你需要重复做这件事吗？请选择频率。',
+          option: {
+            type: 'radio',
+            model:'optionData.times',
+            items:[
+              {label:'一次性',value:'optionData.times[2]'},
+              {label:'每天',value:'optionData.times[0]'},
+              {label:'每周',value:'optionData.times[1]'},
+            ]
+          },
+          rating: {
+            type: 'rating',
+            class:'primary',
+            label:'做成这件事有多难？请选择难度。',
+            model:0,//这里固定写一个值，那用户输入的rating值存在哪里？
+            max:5,
+          },
+          // 在form里面可以用下拉框吗？待确认。
+          // select: {
+          //   type: 'list',
+          //   model:'',
+          //   options:'optionData.times'
+          // },
+        },
+        buttons: [
+          '取消',
+          {
+            label: '完成',
+             handler (data) {
+              Toast.create('Returned ' + JSON.stringify(data))
+             }
+           }
+         ]
+      })
+     }
     }
   }
 
@@ -49,17 +99,17 @@
 
 
 <style>
-  .headerStander{
+  .headerStander {
     padding-right: 1.5rem;
     text-align: center;
   }
-  .addButton{
-    width:100%;
-    margin:2rem 0;
-    padding:1.5rem;
+  .addButton {
+    width: 100%;
+    margin: 2rem 0;
+    padding: 1.5rem;
   }
-  .addButton>i{
-    font-size:2.8rem;
+  .addButton>i {
+    font-size: 2.8rem;
   }
    /*background-color: #A5DEE4;*/
   /*.slider {
@@ -70,7 +120,7 @@
     background-color: white;
   }
   .q-range-handle {
-    width: 100px;
+    width: 150px;
     height: 150px;
     top: -100%;
     border-radius: 0%;
